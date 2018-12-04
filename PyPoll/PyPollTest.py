c5fad44b5945fcdcd33dda5cf0cb42604d5fa6fe
CharCarr <43052094+CharleenCarr@users.noreply.github.com>
2018-12-04 14:23:47 -0500

    Add files via upload

--- PyPoll/PyPollTest.py
+++ PyPoll/PyPollTest.py
@@ -5,9 +5,12 @@ pypollstarter_csv = os.path.join("..", "PyPoll", "election_data.csv")
 
 # List to store data
 total_votes = 0
+candidate_name = []
 candidate_names = []
-total_candidate_votes = {}
+candidate_list = []
+total_candidate_list = []
 total_percentage_votes = {}
+total_candidate_votes = {}
 total_percentage_candidate_votes = {}
 total_percentage_candidate_round = {}
 winner_candidate = ""
@@ -19,37 +22,24 @@ with open(pypollstarter_csv, newline="", encoding='utf-8') as csvfile:
     for row in csvreader:
         total_votes += 1
         candidate_name = row[2]
+        #total_candidate_votes[candidate_name] = 0
         if candidate_name not in candidate_names:
             candidate_names.append(candidate_name)
             total_candidate_votes[candidate_name] = 0
-        total_candidate_votes[candidate_name] += 1
-        # elif candidate_name in candidate_names[0]:
-        #     total_candidate_votes[0] += 1
-        # elif candidate_name in candidate_names[1]:
-        #     total_candidate_votes[1] += 1
-        # elif candidate_name in candidate_names[2]:
-        #     total_candidate_votes[2] += 1
-        # elif candidate_name in candidate_names[3]:
-        #     total_candidate_votes[3] += 1
+        else:
+            total_candidate_votes[candidate_name] += 1
 
-    total_percentage_candidate_votes[candidate_name] = (total_candidate_votes[candidate_name] / total_votes) * 100
-    total_percentage_candidate_round[candidate_name] = round(total_percentage_candidate_votes[candidate_name],3)
+    for name, votes in total_candidate_votes.items():
+        #total_percentage_candidate_votes[candidate_name] = (total_candidate_votes[candidate_name] / total_votes) * 100
+        total_percentage_candidate_round[name] = round((total_candidate_votes[name]/total_votes)*100,3)
 
-    #print(total_candidate_votes)
-    #print(total_percentage_candidate_votes)
     print("Election Results")
     print("------------------------------------------")
     print("Total Votes:  ", total_votes)
     print("------------------------------------------")
-    print(candidate_name, total_percentage_candidate_round[candidate_name], total_candidate_votes[candidate_name])
-
-
-    # print(str(total_percentage_candidate_votes) + str(total_candidate_votes[candidate_name]))
-#     print("Correy:  ", '$' + str(avg_revenue_round))
-#     print("Li:  ", greatest_rev_inc_mon, '$' + str(greatest_rev_inc))
-#     print("O'Tooley:  ", greatest_rev_dec_mon, '$' + str(greatest_rev_dec))
-#         #if candidates == candidatelist.index(candidates):
-#         #total_candidatelist.append(candidatelist)
+    #print(candidate_name, total_percentage_candidate_round[candidate_name], total_candidate_votes[candidate_name])
+    print(total_candidate_votes)
+    print(total_percentage_candidate_round)
 
 output_file = os.path.join("pypollfinal.txt")
 
@@ -58,7 +48,7 @@ with open(output_file, "w", newline="") as datafile:
     datafile.write("--------------------------------------------------" + '\n')
     datafile.write('Total Votes:  {}'.format(total_votes) + '\n')
     datafile.write('--------------------------------------------------' + '\n')
-    datafile.write('Khan:  '.format(total_percentage_candidate_round) .format(total_candidate_votes[candidate_name]) + '\n')
+    datafile.write('Khan:  '.format(total_percentage_candidate_round[name]) .format(total_candidate_votes[name]) + '\n')
     datafile.write('Correy:  '.format(total_percentage_candidate_round) .format(total_candidate_votes[candidate_name]) + '\n')
     datafile.write('Li:  '.format(total_percentage_candidate_round) .format(total_candidate_votes[candidate_name]) + '\n')
     datafile.write("O'Tooley:  ".format(total_percentage_candidate_round) .format(total_candidate_votes[candidate_name]) + '\n')
